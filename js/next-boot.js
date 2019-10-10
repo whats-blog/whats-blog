ff830967fc5b8fe64c38cc8d7c0da341e4013fdd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-10 07:14:35 +0000

    Deploy xinpingwang/whats-blog to github.com/xinpingwang/whats-blog.git:master

--- js/next-boot.js
+++ js/next-boot.js
@@ -1,23 +1,99 @@
-/* global NexT, CONFIG */
+/* global NexT, CONFIG, Velocity */
 
-window.addEventListener('DOMContentLoaded', () => {
+NexT.boot = {};
+
+NexT.boot.registerEvents = function() {
 
   NexT.utils.registerScrollPercent();
   NexT.utils.registerCanIUseTag();
 
   // Mobile top menu bar.
-  $('.site-nav-toggle button').on('click', () => {
-    var $siteNav = $('.site-nav');
+  document.querySelector('.site-nav-toggle button').addEventListener('click', () => {
+    var siteNav = document.querySelector('.site-nav');
     var ON_CLASS_NAME = 'site-nav-on';
-    var isSiteNavOn = $siteNav.hasClass(ON_CLASS_NAME);
-    var animateAction = isSiteNavOn ? 'slideUp' : 'slideDown';
-    var animateCallback = isSiteNavOn ? 'removeClass' : 'addClass';
+    var animateAction = siteNav.classList.contains(ON_CLASS_NAME) ? 'slideUp' : 'slideDown';
 
-    $siteNav.stop()[animateAction]('fast', () => {
-      $siteNav[animateCallback](ON_CLASS_NAME);
+    if (typeof Velocity === 'function') {
+      Velocity(siteNav, animateAction, {
+        duration: 200,
+        complete: function() {
+          siteNav.classList.toggle(ON_CLASS_NAME);
+        }
+      });
+    } else {
+      siteNav.classList.toggle(ON_CLASS_NAME);
+    }
+  });
+
+  var TAB_ANIMATE_DURATION = 200;
+  document.querySelectorAll('.sidebar-nav li').forEach((element, index) => {
+    element.addEventListener('click', event => {
+      var item = event.currentTarget;
+      var activeTabClassName = 'sidebar-nav-active';
+      var activePanelClassName = 'sidebar-panel-active';
+      if (item.classList.contains(activeTabClassName)) return;
+
+      var targets = document.querySelectorAll('.sidebar-panel');
+      var target = targets[index];
+      var currentTarget = targets[1 - index];
+      window.anime({
+        targets : currentTarget,
+        duration: TAB_ANIMATE_DURATION,
+        easing  : 'linear',
+        opacity : 0,
+        complete: () => {
+          // Prevent adding TOC to Overview if Overview was selected when close & open sidebar.
+          currentTarget.classList.remove(activePanelClassName);
+          target.style.opacity = 0;
+          target.classList.add(activePanelClassName);
+          window.anime({
+            targets : target,
+            duration: TAB_ANIMATE_DURATION,
+            easing  : 'linear',
+            opacity : 1
+          });
+        }
+      });
+
+      [...item.parentNode.children].forEach(element => {
+        element.classList.remove(activeTabClassName);
+      });
+      item.classList.add(activeTabClassName);
     });
   });
 
+  window.addEventListener('resize', NexT.utils.initSidebarDimension);
+
+  window.addEventListener('hashchange', () => {
+    var tHash = location.hash;
+    if (tHash !== '' && !tHash.match(/%\S{2}/)) {
+      var target = document.querySelector(`.tabs ul.nav-tabs li a[href="${tHash}"]`);
+      target && target.click();
+    }
+  });
+};
+
+NexT.boot.refresh = function() {
+
+  /**
+   * Register JS handlers by condition option.
+   * Need to add config option in Front-End at 'layout/_partials/head.swig' file.
+   */
+  CONFIG.fancybox && NexT.utils.wrapImageWithFancyBox();
+  CONFIG.mediumzoom && window.mediumZoom('.post-body :not(a) > img, .post-body > img');
+  CONFIG.lazyload && window.lozad('.post-body img').observe();
+  CONFIG.pangu && window.pangu.spacingPage();
+
+  CONFIG.exturl && NexT.utils.registerExtURL();
+  CONFIG.copycode.enable && NexT.utils.registerCopyCode();
+  NexT.utils.registerTabsTag();
+  NexT.utils.registerActiveMenuItem();
+  NexT.utils.registerSidebarTOC();
+  NexT.utils.wrapTableWithBox();
+  NexT.utils.registerVideoIframe();
+};
+
+NexT.boot.motion = function() {
   // Define Motion Sequence & Bootstrap Motion.
   if (CONFIG.motion.enable) {
     NexT.motion.integrator
@@ -29,57 +105,10 @@ window.addEventListener('DOMContentLoaded', () => {
   } else {
     NexT.utils.updateSidebarPosition();
   }
-});
+};
 
-$(document).on('DOMContentLoaded pjax:success', () => {
-
-  if (CONFIG.save_scroll) {
-    // Read position from localStorage
-    var value = localStorage.getItem('scroll' + location.pathname);
-    $('html, body').animate({ scrollTop: value || 0 });
-    // Write position in localStorage
-    NexT.utils.saveScrollTimer = setInterval(() => {
-      localStorage.setItem('scroll' + location.pathname, window.scrollY);
-    }, 1000);
-  }
-
-  /**
-   * Register JS handlers by condition option.
-   * Need to add config option in Front-End at 'layout/_partials/head.swig' file.
-   */
-  CONFIG.fancybox && NexT.utils.wrapImageWithFancyBox();
-  CONFIG.mediumzoom && window.mediumZoom('.post-body img');
-  CONFIG.lazyload && window.lozad('.post-body img').observe();
-  CONFIG.pangu && window.pangu.spacingPage();
-
-  CONFIG.exturl && NexT.utils.registerExtURL();
-  CONFIG.copycode.enable && NexT.utils.registerCopyCode();
-  NexT.utils.registerTabsTag();
-  NexT.utils.registerActiveMenuItem();
-  NexT.utils.embeddedVideoTransformer();
-
-  /**
-   * Init Sidebar & TOC inner dimensions on all pages and for all schemes.
-   * Need for Sidebar/TOC inner scrolling if content taller then viewport.
-   */
-  function updateSidebarHeight(height) {
-    height = height || 'auto';
-    $('.site-overview, .post-toc').css('max-height', height);
-  }
-
-  function initSidebarDimension() {
-
-    window.addEventListener('resize', () => {
-      var sidebarWrapperHeight = document.body.clientHeight - NexT.utils.getSidebarSchemePadding();
-      updateSidebarHeight(sidebarWrapperHeight);
-    });
-    // Initialize Sidebar & TOC Height.
-    updateSidebarHeight(document.body.clientHeight - NexT.utils.getSidebarSchemePadding());
-  }
-  initSidebarDimension();
-
-  function wrapTable() {
-    $('table').not('.gist table').wrap('<div class="table-container"></div>');
-  }
-  wrapTable();
+window.addEventListener('DOMContentLoaded', () => {
+  NexT.boot.registerEvents();
+  NexT.boot.refresh();
+  NexT.boot.motion();
 });
--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,32 +1,28 @@
-/* global NexT, CONFIG */
+/* global NexT, CONFIG, Velocity */
 
 window.addEventListener('DOMContentLoaded', () => {
 
   var sidebarToggleLines = {
     lines: [],
-    push : function(line) {
-      this.lines.push(line);
-    },
-    init: function() {
+    init : function() {
       this.lines.forEach(line => {
-        line.init();
+        line.transform('init');
       });
     },
     arrow: function() {
       this.lines.forEach(line => {
-        line.arrow();
+        line.transform('arrow');
       });
     },
     close: function() {
       this.lines.forEach(line => {
-        line.close();
+        line.transform('close');
       });
     }
   };
 
   function SidebarToggleLine(settings) {
-    this.el = $(settings.el);
-    this.status = $.extend({}, {
+    this.status = Object.assign({
       init: {
         width    : '100%',
         opacity  : 1,
@@ -35,69 +31,51 @@ window.addEventListener('DOMContentLoaded', () => {
         left     : 0
       }
     }, settings.status);
-    this.init = function() {
-      this.transform('init');
-    };
-    this.arrow = function() {
-      this.transform('arrow');
-    };
-    this.close = function() {
-      this.transform('close');
-    };
     this.transform = function(status) {
-      this.el.css(this.status[status]);
+      Object.assign(document.querySelector(settings.el).style, this.status[status]);
     };
   }
 
-  var isRight = CONFIG.sidebar.position !== 'left';
+  var isRight = CONFIG.sidebar.position === 'right';
 
-  var sidebarToggleLine1st = new SidebarToggleLine({
+  sidebarToggleLines.lines = [new SidebarToggleLine({
     el    : '.sidebar-toggle-line-first',
     status: isRight
       ? {
         arrow: {width: '50%', transform: 'rotate(-45deg)', top: '2px'},
         close: {width: '100%', transform: 'rotate(-45deg)', top: '5px'}
-      }
-      : {
+      } : {
         arrow: {width: '50%', transform: 'rotate(45deg)', top: '2px', left: '50%'},
         close: {width: '100%', transform: 'rotate(-45deg)', top: '5px', left: '0px'}
       }
-  });
-  var sidebarToggleLine2nd = new SidebarToggleLine({
+  }), new SidebarToggleLine({
     el    : '.sidebar-toggle-line-middle',
     status: isRight
       ? {
         arrow: {width: '90%'},
         close: {opacity: 0}
-      }
-      : {
+      } : {
         arrow: {width: '90%', left: '2px'},
         close: {opacity: 0, left: '0px'}
       }
-  });
-  var sidebarToggleLine3rd = new SidebarToggleLine({
+  }), new SidebarToggleLine({
     el    : '.sidebar-toggle-line-last',
     status: isRight
       ? {
         arrow: {width: '50%', transform: 'rotate(45deg)', top: '-2px'},
         close: {width: '100%', transform: 'rotate(45deg)', top: '-5px'}
-      }
-      : {
+      } : {
         arrow: {width: '50%', transform: 'rotate(-45deg)', top: '-2px', left: '50%'},
         close: {width: '100%', transform: 'rotate(45deg)', top: '-5px', left: '0px'}
       }
-  });
+  })];
 
-  sidebarToggleLines.push(sidebarToggleLine1st);
-  sidebarToggleLines.push(sidebarToggleLine2nd);
-  sidebarToggleLines.push(sidebarToggleLine3rd);
-
-  var SIDEBAR_WIDTH = CONFIG.sidebar.width || '320px';
-  var SIDEBAR_DISPLAY_DURATION = 200;
+  var SIDEBAR_WIDTH = CONFIG.sidebar.width || 320;
+  var SIDEBAR_DISPLAY_DURATION = 400;
   var mousePos = {}; var touchPos = {};
 
   var sidebarToggleMotion = {
-    sidebarEl       : $('.sidebar'),
+    sidebarEl       : document.querySelector('.sidebar'),
     isSidebarVisible: false,
     init            : function() {
       sidebarToggleLines.init();
@@ -108,12 +86,9 @@ window.addEventListener('DOMContentLoaded', () => {
       document.querySelector('.sidebar-toggle').addEventListener('click', this.clickHandler.bind(this));
       document.querySelector('.sidebar-toggle').addEventListener('mouseenter', this.mouseEnterHandler.bind(this));
       document.querySelector('.sidebar-toggle').addEventListener('mouseleave', this.mouseLeaveHandler.bind(this));
-      this.sidebarEl
-        .on('touchstart', this.touchstartHandler.bind(this))
-        .on('touchend', this.touchendHandler.bind(this))
-        .on('touchmove', event => {
-          event.preventDefault();
-        });
+      this.sidebarEl.addEventListener('touchstart', this.touchstartHandler.bind(this));
+      this.sidebarEl.addEventListener('touchend', this.touchendHandler.bind(this));
+      this.sidebarEl.addEventListener('touchmove', event => event.preventDefault());
       window.addEventListener('sidebar:show', this.showSidebar.bind(this));
       window.addEventListener('sidebar:hide', this.hideSidebar.bind(this));
     },
@@ -124,8 +99,8 @@ window.addEventListener('DOMContentLoaded', () => {
     mouseupHandler: function(event) {
       var deltaX = event.pageX - mousePos.X;
       var deltaY = event.pageY - mousePos.Y;
-      var clickingBlankPart = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY)) < 20 && $(event.target).is('.main');
-      if (this.isSidebarVisible && (clickingBlankPart || $(event.target).is('img.medium-zoom-image, .fancybox img'))) {
+      var clickingBlankPart = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY)) < 20 && event.target.matches('.main');
+      if (this.isSidebarVisible && (clickingBlankPart || event.target.matches('img.medium-zoom-image, .fancybox img'))) {
         this.hideSidebar();
       }
     },
@@ -143,12 +118,12 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     },
     touchstartHandler: function(event) {
-      touchPos.X = event.originalEvent.touches[0].clientX;
-      touchPos.Y = event.originalEvent.touches[0].clientY;
+      touchPos.X = event.touches[0].clientX;
+      touchPos.Y = event.touches[0].clientY;
     },
     touchendHandler: function(event) {
-      var deltaX = event.originalEvent.changedTouches[0].clientX - touchPos.X;
-      var deltaY = event.originalEvent.changedTouches[0].clientY - touchPos.Y;
+      var deltaX = event.changedTouches[0].clientX - touchPos.X;
+      var deltaY = event.changedTouches[0].clientY - touchPos.Y;
       var effectiveSliding = Math.abs(deltaY) < 20 && ((deltaX > 30 && isRight) || (deltaX < -30 && !isRight));
       if (this.isSidebarVisible && effectiveSliding) {
         this.hideSidebar();
@@ -156,73 +131,51 @@ window.addEventListener('DOMContentLoaded', () => {
     },
     showSidebar: function() {
       this.isSidebarVisible = true;
-      var self = this;
-
-      if ($.isFunction($('html').velocity)) {
-        this.sidebarEl.stop().velocity({
-          width: SIDEBAR_WIDTH
-        }, {
-          display : 'block',
-          duration: SIDEBAR_DISPLAY_DURATION,
-          begin   : function() {
-            $('.sidebar .motion-element').not('.site-state').velocity(
-              isRight ? 'transition.slideRightIn' : 'transition.slideLeftIn', {
-                stagger: 50,
-                drag   : true
-              }
-            );
-            $('.site-state').velocity(
-              isRight ? 'transition.slideRightIn' : 'transition.slideLeftIn', {
-                stagger: 50,
-                drag   : true,
-                display: 'flex'
-              }
-            );
-          },
-          complete: function() {
-            self.sidebarEl.addClass('sidebar-active');
-          }
-        });
-      } else {
-        $('.sidebar .motion-element').show();
-        this.sidebarEl.stop().animate({
-          width  : SIDEBAR_WIDTH,
-          display: 'block'
-        }, SIDEBAR_DISPLAY_DURATION, () => {
-          self.sidebarEl.addClass('sidebar-active');
+      this.sidebarEl.classList.add('sidebar-active');
+      if (typeof Velocity === 'function') {
+        Velocity(document.querySelectorAll('.sidebar .motion-element'), isRight ? 'transition.slideRightIn' : 'transition.slideLeftIn', {
+          stagger: 50,
+          drag   : true
         });
       }
 
       sidebarToggleLines.close();
-      NexT.utils.isDesktop() && $('body').stop().animate(isRight ? {'padding-right': SIDEBAR_WIDTH} : {'padding-left': SIDEBAR_WIDTH}, SIDEBAR_DISPLAY_DURATION);
+      NexT.utils.isDesktop() && window.anime(Object.assign({
+        targets : document.body,
+        duration: SIDEBAR_DISPLAY_DURATION,
+        easing  : 'linear'
+      }, isRight ? {
+        'padding-right': SIDEBAR_WIDTH
+      } : {
+        'padding-left': SIDEBAR_WIDTH
+      }));
     },
     hideSidebar: function() {
       this.isSidebarVisible = false;
-      this.sidebarEl.find('.motion-element').hide();
-      this.sidebarEl.stop().animate({width: 0, display: 'none'}).removeClass('sidebar-active');
+      this.sidebarEl.classList.remove('sidebar-active');
 
       sidebarToggleLines.init();
-      NexT.utils.isDesktop() && $('body').stop().animate(isRight ? {'padding-right': 0} : {'padding-left': 0});
-
-      // Prevent adding TOC to Overview if Overview was selected when close & open sidebar.
-      var tocWrap = document.querySelector('.post-toc-wrap');
-      if (tocWrap) {
-        if ($('.site-overview-wrap').css('display') === 'block') {
-          tocWrap.classList.remove('motion-element');
-        } else {
-          tocWrap.classList.add('motion-element');
-        }
-      }
+      NexT.utils.isDesktop() && window.anime(Object.assign({
+        targets : document.body,
+        duration: SIDEBAR_DISPLAY_DURATION,
+        easing  : 'linear'
+      }, isRight ? {
+        'padding-right': 0
+      } : {
+        'padding-left': 0
+      }));
     }
   };
   sidebarToggleMotion.init();
 
   function updateFooterPosition() {
-    var containerHeight = $('#footer').attr('position') ? document.querySelector('.container').height() + $('#footer').outerHeight(true) : document.querySelector('.container').height();
+    var containerHeight = document.querySelector('.container').offsetHeight;
+    var footer = document.getElementById('footer');
+    if (footer.classList.contains('footer-fixed')) containerHeight += footer.outerHeight(true);
     if (containerHeight < window.innerHeight) {
-      $('#footer').css({ 'position': 'fixed', 'bottom': 0, 'left': 0, 'right': 0 }).attr('position', 'fixed');
+      footer.classList.add('footer-fixed');
     } else {
-      $('#footer').removeAttr('style position');
+      footer.classList.remove('footer-fixed');
     }
   }
 
